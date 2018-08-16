62b30aa8a315829f88c26a83bc2a6b93b288c1ef
Fréderic Kinnaer <frederic@censored.be>
2018-08-16 15:09:08 +0200

    update buildscripts to include all archs

--- censored_check_config/buildlocal.sh
+++ censored_check_config/buildlocal.sh
@@ -1,1 +1,1 @@
-docker run --rm --privileged -v ~/.docker:/root/.docker -v $(pwd)/censored_check_config:/data -v /var/run/docker.sock:/var/run/docker.sock homeassistant/amd64-builder --amd64 -t /data
+docker run --rm --privileged -v ~/.docker:/root/.docker -v $(pwd)/censored_check_config:/data -v /var/run/docker.sock:/var/run/docker.sock homeassistant/amd64-builder --addon --all -t /data
