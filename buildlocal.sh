ad006f48d9745e03ed0444004559b3bd9c22e78a
Fréderic Kinnaer <frederic@censored.be>
2018-08-16 14:40:58 +0200

    add check_config files and restructure repo

--- buildlocal.sh
+++
@@ -1,1 +0,0 @@
-docker run --rm --privileged -v ~/.docker:/root/.docker -v $(pwd)/censored_git_pull:/data -v /var/run/docker.sock:/var/run/docker.sock homeassistant/amd64-builder --amd64 -t /data
--- censored_git_pull/README.md
+++
@@ -1,3 +0,0 @@
-Bugfixed to allow for proper "is git repo" detection.
-
-See https://github.com/home-assistant/hassio-addons/issues/372
