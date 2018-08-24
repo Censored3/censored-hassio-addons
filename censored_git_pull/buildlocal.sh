afecee98c00048e3e5b5c7f3cd62320cdfcf7f4a
Fréderic Kinnaer <frederic@censored.be>
2018-08-24 17:56:55 +0200

    update buildlocal.sh

--- censored_git_pull/buildlocal.sh
+++ censored_git_pull/buildlocal.sh
@@ -1,1 +1,9 @@
-docker run --rm --privileged -v ~/.docker:/root/.docker -v $(pwd):/data -v /var/run/docker.sock:/var/run/docker.sock homeassistant/amd64-builder --addon --all -t /data
+#!/bin/bash
+if [ "$1" == "test"]; then
+  BUILDARCH="--amd64"
+else
+  BUILDARCH="--all"
+fi
+docker run --rm --privileged -v ~/.docker:/root/.docker -v $(pwd):/data \
+  -v /var/run/docker.sock:/var/run/docker.sock homeassistant/amd64-builder \
+  --addon "$BUILDARCH" -t /data
